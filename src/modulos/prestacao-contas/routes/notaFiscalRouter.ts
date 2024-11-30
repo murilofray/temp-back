0786a55bb084c02a560e19ea2ba54514dbb6a907
murilofray <murilofray01@gmail.com>
2024-11-30 13:30:30 -0300

    .

--- src/modulos/prestacao-contas/routes/notaFiscalRouter.ts
+++ src/modulos/prestacao-contas/routes/notaFiscalRouter.ts
@@ -13,7 +13,6 @@ router.delete('/:id', authentication.authenticate, notaFiscalController.delete);
 
 // Busca
 router.get('/:id', authentication.authenticate, notaFiscalController.getById);
-router.get('/bem/:id', authentication.authenticate, notaFiscalController.findByBem);
 router.get('/pesquisa/:id', authentication.authenticate, notaFiscalController.findByPesquisa);
 
 export default router;
